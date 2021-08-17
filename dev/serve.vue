decb169eddb32d5bc16fa18ca64153a7d6df027d
bbones <Brotherhood2015>
2021-08-17 10:49:14 +0300

    Initial commit

---
+++ dev/serve.vue
@@ -0,0 +1,18 @@
+<script>
+import Vue from 'vue';
+// Uncomment import and local "components" registration if library is not registered globally.
+// import { AtreeSample } from '@/entry.esm';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  // components: {
+  //  AtreeSample,
+  // }
+});
+</script>
+
+<template>
+  <div id="app">
+    <atree-sample />
+  </div>
+</template>
---
+++ src/lib-components/index.js
@@ -0,0 +1,3 @@
+/* eslint-disable import/prefer-default-export */
+export { default as ATree } from './ATree.vue';
+export { default as SearchBar } from './SearchBar.vue';
